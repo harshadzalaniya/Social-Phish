ca606022f8fdee5a1443dc1f577a855641018ae9
harshadzalaniya <117854994+harshadzalaniya@users.noreply.github.com>
2026-07-15 15:25:26 +0530

    Rename project from mip22 to Social-Phish

--- .manual_attack/__ROOT__/index.php
+++ .manual_attack/__ROOT__/index.php
@@ -9,7 +9,7 @@
 *  the Free Software Foundation, either version 3 of the License, or
 *  (at your option) any later version.
 *
-*  mip22 is distributed in the hope that it will be useful,
+*  Social-Phish is distributed in the hope that it will be useful,
 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *  GNU Affero General Public License for more details.
